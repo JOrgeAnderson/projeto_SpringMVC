3df5b12f1f2835097b95447a9aaec56aa23a7edc
JOrgeAnderson <62161437+JOrgeAnderson@users.noreply.github.com>
2021-03-23 22:24:16 -0300

    Projeto em desenvolvimento!

---
+++ springboot/src/main/java/spring/project/springboot/SpringbootApplication.java
@@ -0,0 +1,21 @@
+package spring.project.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EntityScan(basePackages = "spring.project.springboot.model")
+@ComponentScan(basePackages = {"spring.*"})//tonando visivel todas as package's do projeto
+@EnableJpaRepositories(basePackages = {"spring.project.springboot.repository"})//tornando visivel todas as package's que envolven persistencia
+@EnableTransactionManagement
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
